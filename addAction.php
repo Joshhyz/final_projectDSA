aaa9410906ea8a8e9a3d0f98f64aafed4369c4e2
qkgalias <galiaskarlos09@gmail.com>
2025-01-27 23:09:57 +0800

    commit

--- addAction.php
+++ addAction.php
@@ -5,22 +5,21 @@
 </head>
 <body>
 <?php
-
 // Include the database connection file
 require_once("dbConnection.php");
 
-if ($_SERVER["REQUEST_METHOD"=="POST"]) {
-	// Escape special characters in string for use in SQL statement	
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Escape special characters in strings for use in the SQL statement	
 	$studentID = mysqli_real_escape_string($mysqli, $_POST['studentID']);
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
 	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
 	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+	$contacts = mysqli_real_escape_string($mysqli, $_POST['contacts']);
 	
-	// NEED TO DEBUG 
 	// Check for empty fields
-	if (empty($studentID) || empty($name) || empty($email) || empty($address)) {
+	if (empty($studentID) || empty($name) || empty($age) || empty($email) || empty($contacts)) {
 		if (empty($studentID)) {
-			echo "<font color='red'>Student Id field is empty.</font><br/>";
+			echo "<font color='red'>Student ID field is empty.</font><br/>";
 		}
 		if (empty($name)) {
 			echo "<font color='red'>Name field is empty.</font><br/>";
@@ -30,6 +29,9 @@ if ($_SERVER["REQUEST_METHOD"=="POST"]) {
 		}
 		if (empty($email)) {
 			echo "<font color='red'>Email field is empty.</font><br/>";
+		}
+		if (empty($contacts)) {
+			echo "<font color='red'>Contacts field is empty.</font><br/>";
 		}
 
 		// Show link to the previous page
@@ -41,8 +43,8 @@ if ($_SERVER["REQUEST_METHOD"=="POST"]) {
 			echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
 		} else {
 			// Prepared statement for safer database interaction
-			$stmt = $mysqli->prepare("INSERT INTO users (`id`, `studentID`, `name`, `age`, `gender`, `email`, `address`) VALUES (?, ?, ?, ?, ?, ?, ?)");
-			$stmt->bind_param("sssssss", $studentID, $name, $email, $address);
+			$stmt = $mysqli->prepare("INSERT INTO users (`studentID`, `name`, `age`, `email`, `contacts`) VALUES (?, ?, ?, ?, ?)");
+			$stmt->bind_param("ssiss", $studentID, $name, $age, $email, $contacts);
 			
 			if ($stmt->execute()) {
 				// Display success message
